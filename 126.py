d3062910baad04c0e5bb23f66d5bc58501463d03
Богачева Анастасия <britta18britta@gmail.com>
2024-12-12 19:15:02 +0300

    Add files via upload

---
+++ 126.py
@@ -0,0 +1,10 @@
+quantity_completed_dz = 12
+quantity_haurs = 1.5
+name_course = 'Python'
+time_one_task = quantity_haurs/quantity_completed_dz
+print(time_one_task)
+print('Курс:',name_course,',всего задач:',quantity_completed_dz,
+      ',затрачено часов:',quantity_haurs,',среднее время выполнения',time_one_task)
+
+
+
